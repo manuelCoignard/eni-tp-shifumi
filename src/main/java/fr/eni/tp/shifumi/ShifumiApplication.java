be34083eb18a5bd37e7d9247c3852ae4661b310f
mcoignard2021 <mcoignard2021@16SE19-2WH3P02.ad.campus-eni.fr>
2022-05-19 16:49:42 +0200

    Initial commit

---
+++ src/main/java/fr/eni/tp/shifumi/ShifumiApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.tp.shifumi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShifumiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShifumiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/eni/tp/shifumi/ShifumiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.tp.shifumi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShifumiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
